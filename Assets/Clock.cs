66e197b6dde14ed4ef820f8f520f195b0cecee40
GregorSmirnow96 <GregorSmirnow96@gmail.com>
2024-11-05 14:08:18 -0700

    Added previousTime to the Clock script.

--- Assets/Clock.cs
+++ Assets/Clock.cs
@@ -12,15 +12,19 @@ public class Clock : MonoBehaviour
     public float fastforwardScale = 0.1f;
     public float freezeTime = 25f;
 
+    public float previousTime;
+
     void Awake()
     {
         globalClock = GameObject.Find("CelestialBodies").GetComponent<Clock>();
+        previousTime = time;
     }
 
     void Update()
     {
         if (freezeTime <= 24f && time >= freezeTime) return;
 
+        previousTime = time;
         time += Time.deltaTime * fastforwardScale;
     }
 }
