46a426f5e7faf85dc0b0b981bf4104ad8ccaa260
GregorSmirnow96 <GregorSmirnow96@gmail.com>
2024-11-04 21:43:19 -0700

    Added RemoveGrass script

---
+++ Assets/RemovesGrass.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class RemovesGrass : MonoBehaviour
+{
+    public float radius = 2f;
+
+    void Start()
+    {
+        GameObject grassRemoverPrefab = AssetDatabase.LoadAssetAtPath<GameObject>($"Assets/StixGames/GrassRemovalSphere.prefab");
+        GameObject grassRemover = Instantiate(grassRemoverPrefab, transform);
+        grassRemover.transform.localScale = new Vector3(radius, radius, radius);
+    }
+}
