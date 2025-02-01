310ea93f716df120a93db8b0cfcba2cc825abb35
GregorSmirnow96 <GregorSmirnow96@gmail.com>
2025-02-01 16:05:53 -0700

    Updated ObjectSpawnController

--- Assets/ObjectSpawnController.cs
+++ Assets/ObjectSpawnController.cs
@@ -47,7 +47,49 @@ public class ObjectSpawnController : MonoBehaviour
 
     void SpawnObject(SpawnRequest request)
     {
-        Instantiate(request.prefab, request.position, request.quaternion);
+        GameObject spawnedObject = Instantiate(request.prefab, request.position, request.quaternion);
+        LaunchObject(spawnedObject);
+    }
+
+    private void LaunchObject(GameObject objectToLaunch)
+    {
+        Rigidbody objectRigidbody = objectToLaunch.GetComponent<Rigidbody>();
+
+        if (objectRigidbody == null)
+        {
+            objectToLaunch.AddComponent<Rigidbody>();
+            objectRigidbody = objectToLaunch.GetComponent<Rigidbody>();
+            objectRigidbody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+        }
+
+        MeshCollider collider = objectToLaunch.GetComponent<MeshCollider>();
+        if (collider == null)
+        {
+            Mesh mesh = MeshUtility.GetMeshFromGameObject(objectToLaunch);
+            if (mesh != null)
+            {
+                collider = objectToLaunch.AddComponent<MeshCollider>();
+                collider.sharedMesh = mesh;
+                collider.convex = true;
+            }
+        }
+
+        if (objectRigidbody != null && collider != null)
+        {
+            float yAngle = Random.Range(0, 360);
+            float xAngle = Random.Range(20, 60);
+            float forceMagnitude = Random.Range(3, 7);
+
+            float yRad = yAngle * Mathf.Deg2Rad;
+            float xRad = xAngle * Mathf.Deg2Rad;
+
+            Vector3 direction = new Vector3(
+                Mathf.Cos(yRad) * Mathf.Cos(xRad),
+                Mathf.Sin(xRad),
+                Mathf.Sin(yRad) * Mathf.Cos(xRad));
+
+            objectRigidbody.AddForce(direction * forceMagnitude, ForceMode.Impulse);
+        }
     }
 
     public GameObject SpawnObjectImmediately(GameObject prefab, Vector3 position, Quaternion quaternion)
